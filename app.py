8ecd79ed8eb868dacf5ab3c3ca89ff4ecb0aa578
Aditya Kumar <adityasns77@gmail.com>
2024-07-20 14:24:35 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, request, render_template, jsonify
+from YoutubeTags import videotags
+import random
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/get_tags', methods=['POST'])
+def get_tags():
+    youtube_link = request.form['youtube_link']
+    tags = videotags(youtube_link).split(',')
+    tags = [tag.strip() for tag in tags]  # Remove extra whitespace
+    return render_template('index.html', tags=tags)
+
+@app.route('/shuffle_tags', methods=['POST'])
+def shuffle_tags():
+    tags = request.json.get('tags')
+    random.shuffle(tags)
+    return jsonify(tags)
+
+if __name__ == '__main__':
+    app.run(debug=True)
